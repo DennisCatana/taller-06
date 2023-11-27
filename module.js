03c22df584ed02dffdeabd63b4a1625a5c485fdb
Aldahir Lascano <139184732+LascanoAldahir@users.noreply.github.com>
2023-11-26 21:25:41 -0500

    Update module.js

--- module.js
+++ module.js
@@ -41,3 +41,35 @@ module.exports.LIKES = 150
 
 
 /* ES Moules */
+
+/* ES Moules */
+// Módulos.js
+// Función para cepillar los dientes
+function cepillarDientes() {
+    console.log("¡Cepillando los dientes!");  }
+  
+  // Función para bañarse
+  function banarse() {
+    console.log("Tomando una ducha refrescante.");  }
+  
+  // Función para lavar las manos
+  function lavarManos() {
+    console.log("Lavando las manos con agua y jabón.");  }
+  
+  // Función para vestirse
+  function vestirse() {
+    console.log("Seleccionando la ropa del día.");  }
+  
+  // Exportamos las funciones
+  module.exports = {
+    cepillarDientes,
+    banarse,
+    lavarManos,
+    vestirse};
+  
+
+// Ejecutamos algunas acciones cotidianas
+cepillarDientes();
+banarse();
+lavarManos();
+vestirse();
